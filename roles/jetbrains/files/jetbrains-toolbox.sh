170dcc45b43f2bfa8fffee0d9392f1abd54a8049
Gilles Habran <gilleshabran@protonmail.com>
2022-05-24 19:54:29 +0200

    custom install jetbrains

---
+++ roles/jetbrains/files/jetbrains-toolbox.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+# source: https://github.com/nagygergo/jetbrains-toolbox-install
+
+echo -e " Installing Jetbrains Toolbox"
+echo ""
+
+function getLatestUrl() {
+USER_AGENT=('User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36')
+
+URL=$(curl 'https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release' -H 'Origin: https://www.jetbrains.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.8' -H "${USER_AGENT[@]}" -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://www.jetbrains.com/toolbox/download/' -H 'Connection: keep-alive' -H 'DNT: 1' --compressed | grep -Po '"linux":.*?[^\\]",' | awk -F ':' '{print $3,":"$4}'| sed 's/[", ]//g')
+echo $URL
+}
+getLatestUrl
+
+FILE=$(basename ${URL})
+DEST=/tmp/$FILE
+
+echo ""
+echo -e "Downloading Toolbox files"
+echo ""
+wget -cO  ${DEST} ${URL} --read-timeout=5 --tries=0
+echo ""
+echo -e "Download complete!"
+echo ""
+DIR="/opt/jetbrains-toolbox"
+echo ""
+echo  -e "Installing to $DIR"
+echo ""
+
+if mkdir ${DIR}; then
+    tar -xzf ${DEST} -C ${DIR} --strip-components=1
+fi
+
+chmod -R +rwx ${DIR}
+
+ln -s ${DIR}/jetbrains-toolbox /usr/local/bin/jetbrains-toolbox
+chmod +x /usr/local/bin/jetbrains-toolbox
+
+echo ""
+rm ${DEST}
+echo  -e "Done."
