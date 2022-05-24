5410c9b3e64a152d3a7768f78c43b6fb5668482e
Gilles Habran <63115776+gillouche@users.noreply.github.com>
2022-05-24 21:39:42 +0200

    Update for kali setup (#3)
    Update playbooks for Kali setup

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 verbosity=$1
 
-ansible-playbook -e target_user=$USER -i hosts --ask-vault-pass --ask-become-pass playbook.yml ${verbosity}
+ansible-playbook -e target_user=$USER -i hosts.yml --ask-vault-pass --ask-become-pass site.yml ${verbosity}
 
